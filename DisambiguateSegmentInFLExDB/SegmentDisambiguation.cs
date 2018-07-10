e78a980575ae200660f0e04babceb8de3c985f16
andy black <andy_black@sil.org>
2018-07-10 13:55:51 -0700

    Add disambiguation

---
+++ DisambiguateSegmentInFLExDB/SegmentDisambiguation.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) 2018 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using SIL.LCModel;
+using SIL.LCModel.Infrastructure;
+using SIL.PrepFLExDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIL.DisambiguateSegmentInFLExDB
+{
+	public class SegmentDisambiguation
+	{
+		public SegmentDisambiguation(ISegment segment, List<Guid> disambiguatedMorphBundleGuids)
+		{
+			Segment = segment;
+			DisambiguatedMorphBundles = disambiguatedMorphBundleGuids;
+		}
+
+		public ISegment Segment { get; set; }
+		public List<Guid> DisambiguatedMorphBundles { get; set; }
+
+		public void Disambiguate(LcmCache cache)
+		{
+			var pcpatrAgent = GetPCPATRSyntacticParsingAgent(cache);
+			NonUndoableUnitOfWorkHelper.Do(cache.ActionHandlerAccessor, () =>
+			{
+				int i = 0;
+				foreach (IAnalysis analysis in Segment.AnalysesRS)
+				{
+					if (analysis.ClassID == WfiWordformTags.kClassId)
+					{
+						var wfiWordform = analysis as IWfiWordform;
+						var wfiMorphBundelGuidToUse = DisambiguatedMorphBundles.ElementAt(i);
+						var wfiMorphBundle = cache.ServiceLocator.ObjectRepository.GetObject(wfiMorphBundelGuidToUse);
+						var wfiAnalysisToUse = wfiMorphBundle.Owner as IWfiAnalysis;
+						wfiAnalysisToUse.Analysis.SetAgentOpinion(pcpatrAgent, Opinions.approves);
+						Segment.AnalysesRS.RemoveAt(i);
+						Segment.AnalysesRS.Insert(i, wfiAnalysisToUse);
+					}
+					else if (analysis.ClassID != PunctuationFormTags.kClassId)
+					{
+						analysis.Analysis.SetAgentOpinion(pcpatrAgent, Opinions.approves);
+					}
+					i++;
+				}
+			});
+		}
+
+		public ICmAgent GetPCPATRSyntacticParsingAgent(LcmCache cache)
+		{
+			var agents = cache.LangProject.AnalyzingAgentsOC;
+			var pcpatrAgents = agents.Where(a => a.Name.BestAnalysisAlternative.Text == Constants.PcPatrSyntacticParser);
+			if (pcpatrAgents.Count() > 0)
+			{
+				return pcpatrAgents.First();
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+	}
+}
---
+++ DisambiguateSegmentInFLExDBTests/DisambiguateSegmentTest.cs
@@ -0,0 +1,155 @@
+﻿// Copyright (c) 2018 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using NUnit.Framework;
+using SIL.LcmLoader;
+using SIL.LCModel;
+using SIL.LCModel.Core.Text;
+using SIL.LCModel.DomainServices;
+using SIL.WritingSystems;
+using SIL.DisambiguateSegmentInFLExDB;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIL.DisambiguateSegmentInFLExDBTests
+{
+	[TestFixture]
+	class DisambiguateSegmentTests : MemoryOnlyBackendProviderTestBase
+	{
+		String TestDataDir { get; set; }
+		LcmCache myCache { get; set; }
+		public SIL.LcmLoader.LcmLoader Loader { get; set; }
+		public ProjectId ProjId { get; set; }
+		List<Guid> MorphBundleGuidsWeWantToGetMarriedAndBeHappy { get; set; }
+
+		public override void FixtureSetup()
+		{
+			Icu.InitIcuDataDir();
+			if (!Sldr.IsInitialized)
+			{
+				Sldr.Initialize();
+			}
+
+			base.FixtureSetup();
+			Uri uriBase = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+			var rootdir = Path.GetDirectoryName(Uri.UnescapeDataString(uriBase.AbsolutePath));
+			int i = rootdir.LastIndexOf("DisambiguateSegmentInFLExDBTests");
+			String basedir = rootdir.Substring(0, i);
+			TestDataDir = Path.Combine(basedir, "DisambiguateSegmentInFLExDBTests", "TestData");
+			String savedTestFile = Path.Combine(TestDataDir, "PCPATRTestingB4.fwdata");
+			String testfile = Path.Combine(TestDataDir, "PCPATRTesting.fwdata");
+			File.Copy(savedTestFile, testfile, true);	
+			ProjId = new ProjectId(testfile);
+			Loader = new SIL.LcmLoader.LcmLoader(ProjId);
+			myCache = Loader.CreateCache();
+
+			MorphBundleGuidsWeWantToGetMarriedAndBeHappy = new List<Guid>()
+			{
+				new Guid("e2e4949d-9af0-4142-9d4f-f2d9afdcb646"),
+				new Guid("b3e8623e-5679-4261-acd5-d62ed71d1d2b"),
+				new Guid("2f63f58f-112b-46ab-b329-3dc85ffda392"),
+				new Guid("8ac47da9-e3dd-4645-9ef6-507b4f9dc802"),
+				new Guid("1ea23f59-f6d9-406d-89f6-792318a04efe"),
+				new Guid("479aca02-ca6a-4c2a-862a-d980fbcc9a37"),
+				new Guid("04f021dc-a0dd-44fc-8b0a-9e6741743dd8"),
+				new Guid("07fbf262-bbe7-415b-af3f-8317a2cb4521")
+			};
+		}
+
+		/// <summary></summary>
+		public override void FixtureTeardown()
+		{
+			base.FixtureTeardown();
+			if (myCache != null)
+			{
+				ProjectLockingService.UnlockCurrentProject(myCache);
+			}
+		}
+
+		/// <summary>
+		/// Test extracting of lexicon.
+		/// </summary>
+		[Test]
+		public void DisambiguateSegmentTest()
+		{
+			//myCache = Loader.CreateCache();
+			Assert.IsNotNull(myCache);
+			Assert.AreEqual(ProjId.UiName, myCache.ProjectId.UiName);
+			Assert.AreEqual(26, myCache.LangProject.AllPartsOfSpeech.Count);
+			Assert.AreEqual(323, myCache.LangProject.LexDbOA.Entries.Count());
+			Assert.AreEqual(4, myCache.LangProject.InterlinearTexts.Count);
+
+			var text = myCache.LangProject.InterlinearTexts.Where(t => t.Title.BestAnalysisAlternative.Text == "Part 4").First();
+			var paragraph = (IStTxtPara)text.ParagraphsOS.ElementAt(3);
+			var segment = paragraph.SegmentsOS.First();
+			var segmentDisam = new SegmentDisambiguation(segment, MorphBundleGuidsWeWantToGetMarriedAndBeHappy);
+			var pcpatrAgent = segmentDisam.GetPCPATRSyntacticParsingAgent(myCache);
+
+			// Before disambiguation
+			Assert.AreEqual(9, segment.AnalysesRS.Count);
+			var analysis = segment.AnalysesRS.ElementAt(0); // we
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.noopinion, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(1); // want
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.noopinion, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(2); // to (ambiguous)
+			Assert.AreEqual(WfiWordformTags.kClassId, analysis.ClassID);
+			Assert.IsNull(analysis.Analysis);
+			analysis = segment.AnalysesRS.ElementAt(3); // get (ambiguous)
+			Assert.AreEqual(WfiWordformTags.kClassId, analysis.ClassID);
+			Assert.IsNull(analysis.Analysis);
+			analysis = segment.AnalysesRS.ElementAt(4); // married
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.noopinion, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(5); // and
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.noopinion, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(6); // be (ambiguous)
+			Assert.AreEqual(WfiWordformTags.kClassId, analysis.ClassID);
+			Assert.IsNull(analysis.Analysis);
+			analysis = segment.AnalysesRS.ElementAt(7); // happy
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.noopinion, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+
+			//var segmentDisam = new SegmentDisambiguation(segment, MorphBundleGuidsWeWantToGetMarriedAndBeHappy);
+			segmentDisam.Disambiguate(myCache);
+			//After disambiguation
+			Assert.AreEqual(9, segment.AnalysesRS.Count);
+			analysis = segment.AnalysesRS.ElementAt(0); // we
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(1); // want
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(2); // to (ambiguous)
+			Assert.AreEqual(WfiAnalysisTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(3); // get (ambiguous)
+			Assert.AreEqual(WfiAnalysisTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(4); // married
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(5); // and
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(6); // be (ambiguous)
+			Assert.AreEqual(WfiAnalysisTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+			analysis = segment.AnalysesRS.ElementAt(7); // happy
+			Assert.AreEqual(WfiGlossTags.kClassId, analysis.ClassID);
+			Assert.AreEqual(Opinions.approves, analysis.Analysis.GetAgentOpinion(pcpatrAgent));
+
+
+
+		}
+
+	}
+}
