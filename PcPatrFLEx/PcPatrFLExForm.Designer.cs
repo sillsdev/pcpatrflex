6226b0659cf244b3074205bf859e08683debd890
andy black <andy_black@sil.org>
2018-08-01 09:56:28 -0700

    Show texts and segments for a given language project

--- PcPatrFLEx/PcPatrFLExForm.Designer.cs
+++ PcPatrFLEx/PcPatrFLExForm.Designer.cs
@@ -30,9 +30,17 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PcPatrFLExForm));
 			this.btnParse = new System.Windows.Forms.Button();
-			this.textsPanel = new System.Windows.Forms.Panel();
 			this.btnProject = new System.Windows.Forms.Button();
 			this.lblDatabaseToUse = new System.Windows.Forms.Label();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.lbTexts = new System.Windows.Forms.ListBox();
+			this.lbSegments = new System.Windows.Forms.ListBox();
+			this.lblTexts = new System.Windows.Forms.Label();
+			this.lblSegments = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// btnParse
@@ -43,13 +51,7 @@
 			this.btnParse.TabIndex = 0;
 			this.btnParse.Text = "Parse";
 			this.btnParse.UseVisualStyleBackColor = true;
-			// 
-			// textsPanel
-			// 
-			this.textsPanel.Location = new System.Drawing.Point(1, 63);
-			this.textsPanel.Name = "textsPanel";
-			this.textsPanel.Size = new System.Drawing.Size(799, 385);
-			this.textsPanel.TabIndex = 1;
+			this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
 			// 
 			// btnProject
 			// 
@@ -70,18 +72,80 @@
 			this.lblDatabaseToUse.TabIndex = 3;
 			this.lblDatabaseToUse.Text = "Chosen FLEx Project";
 			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Location = new System.Drawing.Point(4, 100);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.lbTexts);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.lbSegments);
+			this.splitContainer1.Size = new System.Drawing.Size(796, 466);
+			this.splitContainer1.SplitterDistance = 253;
+			this.splitContainer1.TabIndex = 4;
+			// 
+			// lbTexts
+			// 
+			this.lbTexts.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lbTexts.FormattingEnabled = true;
+			this.lbTexts.ItemHeight = 20;
+			this.lbTexts.Location = new System.Drawing.Point(0, 0);
+			this.lbTexts.Name = "lbTexts";
+			this.lbTexts.Size = new System.Drawing.Size(253, 466);
+			this.lbTexts.TabIndex = 1;
+			this.lbTexts.SelectedIndexChanged += new System.EventHandler(this.lbTexts_SelectedIndexChanged_1);
+			// 
+			// lbSegments
+			// 
+			this.lbSegments.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lbSegments.FormattingEnabled = true;
+			this.lbSegments.ItemHeight = 20;
+			this.lbSegments.Location = new System.Drawing.Point(0, 0);
+			this.lbSegments.Name = "lbSegments";
+			this.lbSegments.Size = new System.Drawing.Size(539, 466);
+			this.lbSegments.TabIndex = 2;
+			this.lbSegments.SelectedIndexChanged += new System.EventHandler(this.lbSegments_SelectedIndexChanged);
+			// 
+			// lblTexts
+			// 
+			this.lblTexts.AutoSize = true;
+			this.lblTexts.Location = new System.Drawing.Point(12, 77);
+			this.lblTexts.Name = "lblTexts";
+			this.lblTexts.Size = new System.Drawing.Size(47, 20);
+			this.lblTexts.TabIndex = 3;
+			this.lblTexts.Text = "Texts";
+			// 
+			// lblSegments
+			// 
+			this.lblSegments.AutoSize = true;
+			this.lblSegments.Location = new System.Drawing.Point(282, 77);
+			this.lblSegments.Name = "lblSegments";
+			this.lblSegments.Size = new System.Drawing.Size(82, 20);
+			this.lblSegments.TabIndex = 4;
+			this.lblSegments.Text = "Segments";
+			// 
 			// PcPatrFLExForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(800, 729);
+			this.Controls.Add(this.lblSegments);
+			this.Controls.Add(this.lblTexts);
+			this.Controls.Add(this.splitContainer1);
 			this.Controls.Add(this.lblDatabaseToUse);
 			this.Controls.Add(this.btnProject);
-			this.Controls.Add(this.textsPanel);
 			this.Controls.Add(this.btnParse);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "PcPatrFLExForm";
 			this.Text = "Use PC-PATR with FLEx";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -90,9 +154,13 @@
 		#endregion
 
 		private System.Windows.Forms.Button btnParse;
-		private System.Windows.Forms.Panel textsPanel;
 		private System.Windows.Forms.Button btnProject;
 		private System.Windows.Forms.Label lblDatabaseToUse;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Label lblTexts;
+		private System.Windows.Forms.ListBox lbTexts;
+		private System.Windows.Forms.Label lblSegments;
+		private System.Windows.Forms.ListBox lbSegments;
 	}
 }
 
--- PcPatrFLEx/PcPatrFLExForm.cs
+++ PcPatrFLEx/PcPatrFLExForm.cs
@@ -1,4 +1,5 @@
-﻿using SIL.LcmLoader;
+﻿using SIL.DisambiguateSegmentInFLExDB;
+using SIL.LcmLoader;
 using SIL.LCModel;
 using SIL.LCModel.Core.Text;
 using SIL.LCModel.DomainServices;
@@ -20,6 +21,9 @@ namespace SIL.PcPatrFLEx
 		public LcmCache Cache { get; set; }
 		public ProjectId ProjId { get; set; }
 
+		private IList<IText> Texts { get; set; }
+		private FLExDBExtractor Extractor { get; set; }
+
 		public PcPatrFLExForm()
 		{
 			InitializeComponent();
@@ -27,6 +31,12 @@ namespace SIL.PcPatrFLEx
 			lblDatabaseToUse.Text = "";
 			Icu.InitIcuDataDir();
 			Sldr.Initialize();
+
+			splitContainer1.Dock = DockStyle.Bottom;
+			splitContainer1.Size = new System.Drawing.Size(796, 400);
+
+			lbSegments.DisplayMember = "Baseline";
+			lbSegments.ValueMember = "Segment";
 		}
 
 		private void btnProject_Click(object sender, EventArgs e)
@@ -41,10 +51,18 @@ namespace SIL.PcPatrFLEx
 				Cache = loader.CreateCache();
 				if (Cache != null)
 				{
-					btnParse.Enabled = true;
+					Extractor = new FLExDBExtractor(Cache);
+					FillTextsListBox();
 				}
 			}
 
+		}
+
+		private void FillTextsListBox()
+		{
+			Texts = Cache.LanguageProject.Texts.Where(t => t.ContentsOA != null).Cast<IText>().
+				OrderBy(t => t.ShortName).ToList();
+			lbTexts.DataSource = Texts;
 		}
 
 		private void HandleLcmLoaderEvent(object sender, LcmLoaderEventArgs a)
@@ -87,5 +105,63 @@ namespace SIL.PcPatrFLEx
 			UnlockDatabaseIfNeeded();
 		}
 
+		private void lbTexts_SelectedIndexChanged_1(object sender, EventArgs e)
+		{
+			IList<SegmentToShow> segmentsInListBox = new List<SegmentToShow>();
+			IText text = lbTexts.SelectedItem as IText;
+			var contents = text.ContentsOA;
+			IList<IStPara> paragraphs = contents.ParagraphsOS;
+			foreach (IStPara para in paragraphs)
+			{
+				var paraUse = para as IStTxtPara;
+				if (paraUse != null)
+				{
+					foreach (var segment in paraUse.SegmentsOS)
+					{
+						segmentsInListBox.Add(new SegmentToShow(segment, segment.BaselineText.Text));
+					}
+				}
+			}
+			lbSegments.DataSource = segmentsInListBox;
+		}
+
+		private void btnParse_Click(object sender, EventArgs e)
+		{
+			string ana = GetAnaForm();
+			Console.WriteLine("ana='" + ana + "'");
+		}
+
+		private string GetAnaForm()
+		{
+			var selectedSegmentToShow = (SegmentToShow)lbSegments.SelectedItem;
+			var segment = selectedSegmentToShow.Segment;
+			var ana = Extractor.ExtractTextSegmentAsANA(segment);
+			return ana;
+		}
+
+		private void lbSegments_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var ana = GetAnaForm();
+			if (ana.Contains("\\a \n"))
+			{
+				btnParse.Enabled = false;
+			}
+			else
+			{
+				btnParse.Enabled = true;
+			}
+		}
+	}
+
+	public class SegmentToShow
+	{
+		public ISegment Segment { get; set; }
+		public String Baseline { get; set; }
+
+		public SegmentToShow(ISegment segment, string baseline)
+		{
+			Segment = segment;
+			Baseline = baseline;
+		}
 	}
 }
