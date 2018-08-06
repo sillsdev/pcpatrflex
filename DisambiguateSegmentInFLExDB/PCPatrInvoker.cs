c8871d5652e39047dea50f648b5683cc151af3ce
andy black <andy_black@sil.org>
2018-08-06 11:43:39 -0700

    Add parser invoker

---
+++ DisambiguateSegmentInFLExDB/PCPatrInvoker.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) 2018 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace SIL.DisambiguateSegmentInFLExDB
+{
+	public class PCPatrInvoker
+	{
+		const string takeFileName = "PcPatrFLEx.tak";
+		const string logFileName = "Invoker.log";
+		public String GrammarFile { get; set; }
+		public String AnaFile { get; set; }
+		public String AndFile { get; set; }
+		public String LogFile { get; set; }
+		public String BatchFile { get; set; }
+
+		public PCPatrInvoker(string grammarFile, string anaFile)
+		{
+			GrammarFile = grammarFile;
+			AnaFile = anaFile;
+			LogFile = Path.Combine(Path.GetTempPath(), logFileName);
+		}
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		private static extern int GetShortPathName(String pathName, StringBuilder shortName, int cbShortName);
+
+		private void CreateBatchFile()
+		{
+			BatchFile = Path.Combine(Path.GetTempPath(), "PcPatrFLEx.bat");
+			StringBuilder sbBatchFile = new StringBuilder();
+			sbBatchFile.Append("@echo off\n");
+			sbBatchFile.Append("cd \"");
+			sbBatchFile.Append(Path.GetTempPath());
+			sbBatchFile.Append("\"\n");
+			sbBatchFile.Append("pcpatr32 -t ");
+			sbBatchFile.Append(takeFileName);
+			sbBatchFile.Append("\n");
+			//Console.Write(sbBatchFile.ToString());
+			File.WriteAllText(BatchFile ,sbBatchFile.ToString());
+		}
+		public void Invoke()
+		{
+			CreateBatchFile();
+			CreateTakeFile();
+
+			File.Delete(AndFile);
+			File.Delete(LogFile);
+
+			var processInfo = new ProcessStartInfo("cmd.exe", "/c\"" + BatchFile + "\"");
+			processInfo.CreateNoWindow = true;
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardError = true;
+			processInfo.RedirectStandardOutput = true;
+
+			var process = Process.Start(processInfo);
+			process.Start();
+			process.WaitForExit();
+			string output = process.StandardOutput.ReadToEnd();
+			string error = process.StandardError.ReadToEnd();
+			//Console.Write(output);
+			//Console.Write(error);
+		}
+
+		private void CreateTakeFile()
+		{
+			String takeFile = Path.Combine(Path.GetTempPath(), takeFileName);
+			StringBuilder sbTakeFileShortPath = new StringBuilder(255);
+			int i = GetShortPathName(takeFile, sbTakeFileShortPath, sbTakeFileShortPath.Capacity);
+			var sbTake = new StringBuilder();
+			sbTake.Append("set comment |\n");
+			sbTake.Append("log ");
+			sbTake.Append(logFileName);
+			sbTake.Append("\n");
+			sbTake.Append("load grammar ");
+			StringBuilder sbGrammarFileShortPath = new StringBuilder(255);
+			i = GetShortPathName(GrammarFile, sbGrammarFileShortPath, sbGrammarFileShortPath.Capacity);
+			sbTake.Append(sbGrammarFileShortPath.ToString() + "\n");
+			sbTake.Append("set timing on\n");
+			sbTake.Append("set gloss on\n");
+			sbTake.Append("set features all\n");
+			sbTake.Append("set tree xml\n");
+			sbTake.Append("set ambiguities 100\n");
+			sbTake.Append("set write-ample-parses on\n");
+			sbTake.Append("file disambiguate ");
+			StringBuilder sbAnaFileShortPath = new StringBuilder(255);
+			i = GetShortPathName(AnaFile, sbAnaFileShortPath, sbAnaFileShortPath.Capacity);
+			String anashort = sbAnaFileShortPath.ToString();
+			sbTake.Append(anashort);
+			sbTake.Append(" ");
+			String andshort = sbTakeFileShortPath.ToString();
+			String result = andshort.Substring(0, andshort.Length - 3) + "and";
+			sbTake.Append(result + "\n");
+			sbTake.Append("exit\n");
+			//Console.Write(sbTake.ToString());
+			File.WriteAllText(takeFile, sbTake.ToString());
+			AndFile = result;
+		}
+	}
+}
---
+++ DisambiguateSegmentInFLExDBTests/PcPatrInvokerTests.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2018 SIL International
+// This software is licensed under the LGPL, version 2.1 or later
+// (http://www.gnu.org/licenses/lgpl-2.1.html)
+
+using NUnit.Framework;
+using SIL.DisambiguateSegmentInFLExDB;
+using SIL.LCModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIL.DisambiguateSegmentInFLExDBTest
+{
+	[TestFixture]
+	class PcPatrInvokerTests
+	{
+		String TestDataDir { get; set; }
+		String AnaString { get; set; }
+		String AndString { get; set; }
+
+		[TestFixtureSetUp]
+		public void FixtureSetup()
+		{
+			Uri uriBase = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+			var rootdir = Path.GetDirectoryName(Uri.UnescapeDataString(uriBase.AbsolutePath));
+			int i = rootdir.LastIndexOf("DisambiguateSegmentInFLExDBTests");
+			String basedir = rootdir.Substring(0, i);
+			TestDataDir = Path.Combine(basedir, "DisambiguateSegmentInFLExDBTests", "TestData");
+		}
+
+		/// <summary></summary>
+		[TestFixtureTearDown]
+		public void FixtureTeardown()
+		{
+		}
+
+		/// <summary>
+		/// Test extracting of lexicon.
+		/// </summary>
+		[Test]
+		public void PcPatrInvokerTest()
+		{
+			String grammarFile = Path.Combine(TestDataDir, "Invoker.grm");
+			String anaFile = Path.Combine(TestDataDir, "Invoker.ana");
+			String andFile = Path.Combine(TestDataDir, "Invoker.and");
+			using (var streamReader = new StreamReader(andFile, Encoding.UTF8))
+			{
+				AndString = streamReader.ReadToEnd().Replace("\r", "");
+			}
+			var invoker = new PCPatrInvoker(grammarFile, anaFile);
+			invoker.Invoke();
+			String andResult = "";
+			using (var streamReader = new StreamReader(invoker.AndFile, Encoding.UTF8))
+			{
+				andResult = streamReader.ReadToEnd().Replace("\r", "");
+			}
+			// The \id line has the location of the Invoker.grm file which will vary by machine.
+			// So we just check the firset 23 characters (which are always the same)
+			// and what starts at "Invoker.grm".
+			int iIDBeginning = 23;
+			int iExpected = AndString.IndexOf("Invoker.grm");
+			int iResult = andResult.IndexOf("Invoker.grm");
+			Assert.AreEqual(AndString.Substring(0, iIDBeginning), andResult.Substring(0, iIDBeginning));
+			Assert.AreEqual(AndString.Substring(iExpected), andResult.Substring(iResult));
+		}
+	}
+}
