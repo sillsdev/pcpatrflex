b3b2ac3e765df3d70dea8c0d08b2e83ad1e3236f
andy black <andy_black@sil.org>
2018-10-08 14:28:14 -0700

    First pass at making a DLL that can be included with FLEx

---
+++ PcPatrFLExDll/PcPatrFLExForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace SIL.PcPatrFLEx
+{
+	partial class PcPatrFLExForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PcPatrFLExForm));
+			this.btnParse = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.lbTexts = new System.Windows.Forms.ListBox();
+			this.lbSegments = new System.Windows.Forms.ListBox();
+			this.lblTexts = new System.Windows.Forms.Label();
+			this.lblSegments = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tbGrammarFile = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.btnDisambiguate = new System.Windows.Forms.Button();
+			this.btnHelp = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnParse
+			// 
+			this.btnParse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnParse.Location = new System.Drawing.Point(519, 112);
+			this.btnParse.Name = "btnParse";
+			this.btnParse.Size = new System.Drawing.Size(267, 38);
+			this.btnParse.TabIndex = 10;
+			this.btnParse.Text = "&Parse && show this segment";
+			this.btnParse.UseVisualStyleBackColor = true;
+			this.btnParse.Click += new System.EventHandler(this.Parse_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.splitContainer1.Location = new System.Drawing.Point(3, 160);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.lbTexts);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.lbSegments);
+			this.splitContainer1.Size = new System.Drawing.Size(796, 790);
+			this.splitContainer1.SplitterDistance = 356;
+			this.splitContainer1.TabIndex = 4;
+			this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
+			// 
+			// lbTexts
+			// 
+			this.lbTexts.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lbTexts.FormattingEnabled = true;
+			this.lbTexts.ItemHeight = 20;
+			this.lbTexts.Location = new System.Drawing.Point(0, 0);
+			this.lbTexts.Name = "lbTexts";
+			this.lbTexts.Size = new System.Drawing.Size(356, 790);
+			this.lbTexts.TabIndex = 0;
+			this.lbTexts.SelectedIndexChanged += new System.EventHandler(this.Texts_SelectedIndexChanged);
+			// 
+			// lbSegments
+			// 
+			this.lbSegments.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lbSegments.FormattingEnabled = true;
+			this.lbSegments.ItemHeight = 20;
+			this.lbSegments.Location = new System.Drawing.Point(0, 0);
+			this.lbSegments.Name = "lbSegments";
+			this.lbSegments.Size = new System.Drawing.Size(436, 790);
+			this.lbSegments.TabIndex = 0;
+			this.lbSegments.SelectedIndexChanged += new System.EventHandler(this.Segments_SelectedIndexChanged);
+			// 
+			// lblTexts
+			// 
+			this.lblTexts.AutoSize = true;
+			this.lblTexts.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTexts.Location = new System.Drawing.Point(9, 122);
+			this.lblTexts.Name = "lblTexts";
+			this.lblTexts.Size = new System.Drawing.Size(61, 25);
+			this.lblTexts.TabIndex = 7;
+			this.lblTexts.Text = "Texts";
+			// 
+			// lblSegments
+			// 
+			this.lblSegments.AutoSize = true;
+			this.lblSegments.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSegments.Location = new System.Drawing.Point(367, 122);
+			this.lblSegments.Name = "lblSegments";
+			this.lblSegments.Size = new System.Drawing.Size(101, 25);
+			this.lblSegments.TabIndex = 9;
+			this.lblSegments.Text = "Segments";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(180, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "PC-PATR Grammar file: ";
+			// 
+			// tbGrammarFile
+			// 
+			this.tbGrammarFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbGrammarFile.Location = new System.Drawing.Point(216, 28);
+			this.tbGrammarFile.Name = "tbGrammarFile";
+			this.tbGrammarFile.Size = new System.Drawing.Size(885, 26);
+			this.tbGrammarFile.TabIndex = 1;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnBrowse.Location = new System.Drawing.Point(1128, 28);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(94, 38);
+			this.btnBrowse.TabIndex = 2;
+			this.btnBrowse.Text = "&Browse";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.Browse_Click);
+			// 
+			// btnDisambiguate
+			// 
+			this.btnDisambiguate.Location = new System.Drawing.Point(77, 112);
+			this.btnDisambiguate.Name = "btnDisambiguate";
+			this.btnDisambiguate.Size = new System.Drawing.Size(282, 38);
+			this.btnDisambiguate.TabIndex = 8;
+			this.btnDisambiguate.Text = "&Disambiguate && show this text";
+			this.btnDisambiguate.UseVisualStyleBackColor = true;
+			this.btnDisambiguate.Click += new System.EventHandler(this.Disambiguate_Click);
+			// 
+			// btnHelp
+			// 
+			this.btnHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnHelp.Location = new System.Drawing.Point(1146, 96);
+			this.btnHelp.Name = "btnHelp";
+			this.btnHelp.Size = new System.Drawing.Size(76, 40);
+			this.btnHelp.TabIndex = 6;
+			this.btnHelp.Text = "Help...";
+			this.btnHelp.UseVisualStyleBackColor = true;
+			this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+			// 
+			// PcPatrFLExForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1228, 947);
+			this.Controls.Add(this.btnHelp);
+			this.Controls.Add(this.btnDisambiguate);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.tbGrammarFile);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblSegments);
+			this.Controls.Add(this.lblTexts);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.btnParse);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "PcPatrFLExForm";
+			this.Text = "Use PC-PATR with FLEx";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnFormClosing);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnParse;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Label lblTexts;
+		private System.Windows.Forms.ListBox lbTexts;
+		private System.Windows.Forms.Label lblSegments;
+		private System.Windows.Forms.ListBox lbSegments;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tbGrammarFile;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.Button btnDisambiguate;
+		private System.Windows.Forms.Button btnHelp;
+	}
+}
+
---
+++ PcPatrFLExDll/PcPatrFLExForm.cs
@@ -0,0 +1,498 @@
+﻿using Microsoft.Win32;
+using SIL.DisambiguateInFLExDB;
+using SIL.LCModel;
+using SIL.LCModel.Core.Text;
+using SIL.LCModel.Core.WritingSystems;
+using SIL.LCModel.DomainServices;
+using SIL.PcPatrBrowser;
+using SIL.PrepFLExDB;
+using SIL.WritingSystems;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIL.PcPatrFLEx
+{
+	public partial class PcPatrFLExForm : Form
+	{
+		public LcmCache Cache { get; set; }
+
+		private IList<IText> Texts { get; set; }
+		private IList<SegmentToShow> SegmentsInListBox { get; set; }
+		private FLExDBExtractor Extractor { get; set; }
+		private String GrammarFile { get; set; }
+		private Font AnalysisFont { get; set; }
+		private Font VernacularFont { get; set; }
+
+		public static string m_strRegKey = "Software\\SIL\\PcPatrFLEx";
+		const string m_strLastDatabase = "LastDatabase";
+		const string m_strLastGrammarFile = "LastGrammarFile";
+		const string m_strLastText = "LastText";
+		const string m_strLastSegment = "LastSegment";
+		const string m_strLocationX = "LocationX";
+		const string m_strLocationY = "LocationY";
+		const string m_strSizeHeight = "SizeHeight";
+		const string m_strSizeWidth = "SizeWidth";
+		const string m_strWindowState = "WindowState";
+
+		public Rectangle RectNormal { get; set; }
+
+		public string LastDatabase { get; set; }
+		public string LastGrammarFile { get; set; }
+		public string LastText { get; set; }
+		public string LastSegment { get; set; }
+		public string RetrievedLastText { get; set; }
+		public string RetrievedLastSegment { get; set; }
+
+		private RegistryKey regkey;
+
+		private ContextMenuStrip helpContextMenu;
+		const string UserDocumentation = "User Documentation";
+		const string PCPATRReferenceManual = "PC-PATR Reference Manual";
+		const string About = "About";
+
+		public PcPatrFLExForm()
+		{
+			InitializeComponent();
+			btnParse.Enabled = false;
+
+			splitContainer1.Dock = DockStyle.Bottom;
+			splitContainer1.SplitterWidth = 3;
+
+			lbSegments.DisplayMember = "Baseline";
+			lbSegments.ValueMember = "Segment";
+
+			BuildHelpContextMenu();
+
+			try
+			{
+				regkey = Registry.CurrentUser.OpenSubKey(m_strRegKey);
+				if (regkey != null)
+				{
+					Cursor.Current = Cursors.WaitCursor;
+					Application.DoEvents();
+					retrieveRegistryInfo();
+					regkey.Close();
+					DesktopBounds = RectNormal;
+					WindowState = WindowState;
+					StartPosition = FormStartPosition.Manual;
+					if (!String.IsNullOrEmpty(LastGrammarFile))
+						tbGrammarFile.Text = LastGrammarFile;
+					if (Cache != null && Cache.LangProject.Texts.Count > 0)
+						btnDisambiguate.Enabled = true;
+					else
+						btnDisambiguate.Enabled = false;
+					Cursor.Current = Cursors.Default;
+				}
+				AdjustSplitterLocation();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.InnerException);
+				Console.WriteLine(e.StackTrace);
+			}
+		}
+
+		private void AdjustSplitterLocation()
+		{
+			int x = splitContainer1.Location.X;
+			// Make the Y location be where the bottom of the disambiguate
+			//  button is plus a gap of 15 pixels
+			int y = this.ClientSize.Height - (btnDisambiguate.Bottom + 15);
+			splitContainer1.Size = new Size(x, y);
+		}
+
+		private void BuildHelpContextMenu()
+		{
+			helpContextMenu = new ContextMenuStrip();
+			ToolStripMenuItem userDoc = new ToolStripMenuItem(UserDocumentation);
+			userDoc.Click += new EventHandler(UserDoc_Click);
+			userDoc.Name = UserDocumentation;
+			ToolStripMenuItem pcpatrDoc = new ToolStripMenuItem(PCPATRReferenceManual);
+			pcpatrDoc.Click += new EventHandler(PCPATRDoc_Click);
+			pcpatrDoc.Name = PCPATRReferenceManual;
+			ToolStripMenuItem about = new ToolStripMenuItem(About);
+			about.Click += new EventHandler(About_Click);
+			about.Name = About;
+			helpContextMenu.Items.Add(userDoc);
+			helpContextMenu.Items.Add(pcpatrDoc);
+			helpContextMenu.Items.Add("-");
+			helpContextMenu.Items.Add(about);
+		}
+
+		void retrieveRegistryInfo()
+		{
+			// Window location
+			int iX = (int)regkey.GetValue(m_strLocationX, 100);
+			int iY = (int)regkey.GetValue(m_strLocationY, 100);
+			int iWidth = (int)regkey.GetValue(m_strSizeWidth, 809); // 1228);
+			int iHeight = (int)regkey.GetValue(m_strSizeHeight, 670); // 947);
+			RectNormal = new Rectangle(iX, iY, iWidth, iHeight);
+			// Set form properties
+			WindowState = (FormWindowState)regkey.GetValue(m_strWindowState, 0);
+
+			LastDatabase = (string)regkey.GetValue(m_strLastDatabase);
+			GrammarFile = LastGrammarFile = (string)regkey.GetValue(m_strLastGrammarFile);
+			RetrievedLastText = LastText = (string)regkey.GetValue(m_strLastText);
+			RetrievedLastSegment = LastSegment = (string)regkey.GetValue(m_strLastSegment);
+		}
+		public void saveRegistryInfo()
+		{
+			regkey = Registry.CurrentUser.OpenSubKey(m_strRegKey, true);
+			if (regkey == null)
+			{
+				regkey = Registry.CurrentUser.CreateSubKey(m_strRegKey);
+			}
+
+			if (LastDatabase != null)
+				regkey.SetValue(m_strLastDatabase, LastDatabase);
+			if (LastGrammarFile != null)
+				regkey.SetValue(m_strLastGrammarFile, LastGrammarFile);
+			if (LastText != null)
+				regkey.SetValue(m_strLastText, LastText);
+			if (LastSegment != null)
+				regkey.SetValue(m_strLastSegment, LastSegment);
+			// Window position and location
+			regkey.SetValue(m_strWindowState, (int)WindowState);
+			regkey.SetValue(m_strLocationX, RectNormal.X);
+			regkey.SetValue(m_strLocationY, RectNormal.Y);
+			regkey.SetValue(m_strSizeWidth, RectNormal.Width);
+			regkey.SetValue(m_strSizeHeight, RectNormal.Height);
+
+			regkey.Close();
+		}
+
+		private static Font CreateFont(CoreWritingSystemDefinition wsDef)
+		{
+			float fontSize = (wsDef.DefaultFontSize == 0) ? 10 : wsDef.DefaultFontSize;
+			var fStyle = FontStyle.Regular;
+			if (wsDef.DefaultFontFeatures.Contains("Bold"))
+			{
+				fStyle |= FontStyle.Bold;
+			}
+			if (wsDef.DefaultFontFeatures.Contains("Italic"))
+			{
+				fStyle |= FontStyle.Italic;
+			}
+			return new Font(wsDef.DefaultFontName, fontSize, fStyle);
+		}
+
+		private void EnsureDatabaseHasBeenPrepped()
+		{
+			var preparer = new Preparer(Cache, false);
+			preparer.AddPCPATRList();
+			preparer.AddPCPATRSenseCustomField();
+		}
+
+		public void PrepareForm()
+		{
+			if (Cache != null)
+			{
+				EnsureDatabaseHasBeenPrepped();
+				Extractor = new FLExDBExtractor(Cache);
+				AnalysisFont = CreateFont(Cache.LanguageProject.DefaultAnalysisWritingSystem);
+				lbTexts.Font = AnalysisFont;
+				VernacularFont = CreateFont(Cache.LanguageProject.DefaultVernacularWritingSystem);
+				lbSegments.Font = VernacularFont;
+				FillTextsListBox();
+			}
+		}
+
+		public void FillTextsListBox()
+		{
+			Texts = Cache.LanguageProject.Texts.Where(t => t.ContentsOA != null).Cast<IText>().
+				OrderBy(t => t.ShortName).ToList();
+			lbTexts.DataSource = Texts;
+			if (Texts.Count > 0)
+				btnDisambiguate.Enabled = true;
+			// select last used text and segment, if any
+			if (!String.IsNullOrEmpty(LastText))
+			{
+				var selectedText = Texts.Where(t => t.Guid.ToString() == RetrievedLastText).FirstOrDefault();
+				if (selectedText != null)
+				{
+					lbTexts.SelectedIndex = Texts.IndexOf(selectedText);
+				}
+				var selectedSegment = SegmentsInListBox.Where(s => s.Segment.Guid.ToString() == RetrievedLastSegment).FirstOrDefault();
+				if (selectedSegment != null)
+				{
+					lbSegments.SelectedIndex = SegmentsInListBox.IndexOf(selectedSegment);
+				}
+			}
+		}
+
+		private void OnFormClosing(object sender, EventArgs e)
+		{
+			Console.WriteLine("form closing");
+			saveRegistryInfo();
+		}
+
+		private void Texts_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			SegmentsInListBox = new List<SegmentToShow>();
+			IText text = lbTexts.SelectedItem as IText;
+			LastText = text.Guid.ToString();
+			var contents = text.ContentsOA;
+			IList<IStPara> paragraphs = contents.ParagraphsOS;
+			foreach (IStPara para in paragraphs)
+			{
+				var paraUse = para as IStTxtPara;
+				if (paraUse != null)
+				{
+					foreach (var segment in paraUse.SegmentsOS)
+					{
+						SegmentsInListBox.Add(new SegmentToShow(segment, segment.BaselineText.Text));
+					}
+				}
+			}
+			lbSegments.DataSource = SegmentsInListBox;
+		}
+
+		private void Parse_Click(object sender, EventArgs e)
+		{
+			Cursor.Current = Cursors.WaitCursor;
+			Application.DoEvents();
+			var selectedSegmentToShow = (SegmentToShow)lbSegments.SelectedItem;
+			string ana = GetAnaForm(selectedSegmentToShow);
+			string andResult;
+			PcPatrBrowserApp browser;
+			var grammarOK = ProcessANAFileAndShowResults(ana, out andResult, out browser);
+			if (grammarOK)
+			{
+				var result = browser.PropertiesChosen;
+				DisambiguateSegment(selectedSegmentToShow, result[0]);
+			}
+			Cursor.Current = Cursors.Default;
+		}
+
+		private PcPatrBrowserApp ShowPcPatrBrowser(string andResult)
+		{
+			var browser = new PcPatrBrowserApp();
+			browser.AdjustUIForPcPatrFLEx();
+			browser.LanguageInfo.GlossFontFace = AnalysisFont.FontFamily.Name;
+			browser.LanguageInfo.GlossFontSize = AnalysisFont.Size;
+			browser.LanguageInfo.GlossFontStyle = AnalysisFont.Style;
+			browser.LanguageInfo.LexFontFace = VernacularFont.FontFamily.Name;
+			browser.LanguageInfo.LexFontSize = VernacularFont.Size;
+			browser.LanguageInfo.LexFontStyle = VernacularFont.Style;
+			browser.LanguageInfo.UseRTL = Cache.LanguageProject.DefaultVernacularWritingSystem.RightToLeftScript;
+			browser.LoadAnaFile(andResult);
+			browser.ShowDialog();
+			return browser;
+		}
+
+		private void DisambiguateSegment(SegmentToShow selectedSegmentToShow, string result)
+		{
+			if (!String.IsNullOrEmpty(result))
+			{
+				var guids = DisambiguateInFLExDB.GuidConverter.CreateListFromString(result);
+				var disambiguator = new SegmentDisambiguation(selectedSegmentToShow.Segment, guids);
+				disambiguator.Disambiguate(Cache);
+			}
+		}
+
+		private string GetAnaForm(SegmentToShow selectedSegmentToShow)
+		{
+			var segment = selectedSegmentToShow.Segment;
+			var ana = Extractor.ExtractTextSegmentAsANA(segment);
+			return ana;
+		}
+
+		private string GetAnaForm(IText selectedTextToShow)
+		{
+			var sb = new StringBuilder();
+			var contents = selectedTextToShow.ContentsOA;
+			IList<IStPara> paragraphs = contents.ParagraphsOS;
+			foreach (IStPara para in paragraphs)
+			{
+				var paraUse = para as IStTxtPara;
+				if (paraUse != null)
+				{
+					foreach (var segment in paraUse.SegmentsOS)
+					{
+						var ana = Extractor.ExtractTextSegmentAsANA(segment);
+						sb.Append(ana.Substring(0, ana.Length-1)); // skip final extra nl
+						// Now add period so PcPatr will treat it as an end of a sentence
+						sb.Append("\\n .\n\n");
+					}
+				}
+			}
+			return sb.ToString();
+		}
+
+		private void Segments_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var selectedSegmentToShow = (SegmentToShow)lbSegments.SelectedItem;
+			LastSegment = selectedSegmentToShow.Segment.Guid.ToString();
+			var ana = GetAnaForm(selectedSegmentToShow);
+			if (ana.Contains("\\a \n"))
+			{
+				btnParse.Enabled = false;
+			}
+			else
+			{
+				btnParse.Enabled = true;
+			}
+		}
+
+		private void Browse_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "PC-PATR Grammar File (*.grm)|*.grm|" +
+			"All Files (*.*)|*.*";
+			if (dlg.ShowDialog() == DialogResult.OK)
+			{
+				GrammarFile = dlg.FileName;
+				LastGrammarFile = GrammarFile;
+				tbGrammarFile.Text = GrammarFile;
+			}
+		}
+
+		protected override void OnMove(EventArgs ea)
+		{
+			base.OnMove(ea);
+
+			if (WindowState == FormWindowState.Normal)
+				RectNormal = DesktopBounds;
+		}
+		protected override void OnResize(EventArgs ea)
+		{
+			base.OnResize(ea);
+
+			if (WindowState == FormWindowState.Normal)
+				RectNormal = DesktopBounds;
+			AdjustSplitterLocation();
+		}
+
+		private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+		{
+			lblSegments.Left = e.SplitX + 10;
+			btnParse.Left = lblSegments.Right + 10;
+		}
+
+		private void Disambiguate_Click(object sender, EventArgs e)
+		{
+			Cursor.Current = Cursors.WaitCursor;
+			Application.DoEvents();
+			var selectedTextToShow = lbTexts.SelectedItem as IText;
+			string ana = GetAnaForm(selectedTextToShow);
+			string andResult;
+			PcPatrBrowserApp browser;
+			var grammarOK = ProcessANAFileAndShowResults(ana, out andResult, out browser);
+			if (grammarOK)
+			{
+				var textdisambiguator = new TextDisambiguation(selectedTextToShow, browser.PropertiesChosen, andResult);
+				textdisambiguator.Disambiguate(Cache);
+			}
+			Cursor.Current = Cursors.Default;
+		}
+
+		private bool ProcessANAFileAndShowResults(string ana, out string andResult, out PcPatrBrowserApp browser)
+		{
+			String anaFile = Path.Combine(Path.GetTempPath(), "Invoker.ana");
+			File.WriteAllText(anaFile, ana);
+			var invoker = new PCPatrInvoker(GrammarFile, anaFile);
+			invoker.Invoke();
+			andResult = invoker.AndFile;
+			if (File.Exists(andResult))
+			{
+				browser = ShowPcPatrBrowser(andResult);
+				return true;
+			}
+			else
+			{
+				browser = null;
+				MessageBox.Show("The PC-PATR grammar file had an error in it and failed to load.\nWe will show the error log after you click on OK.\nPlease fix all errors in the grammar file and then try again.",
+					"Grammar Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Process.Start(andResult.Replace(".and", ".log"));
+				return false;
+			}
+		}
+
+		private void btnHelp_Click(object sender, EventArgs e)
+		{
+			Button btnSender = (Button)sender;
+			Point ptLowerLeft = new Point(0, btnSender.Height);
+			ptLowerLeft = btnSender.PointToScreen(ptLowerLeft);
+			helpContextMenu.Show(ptLowerLeft);
+		}
+
+		void UserDoc_Click(object sender, EventArgs e)
+		{
+			ToolStripItem menuItem = (ToolStripItem)sender;
+			if (menuItem.Name == UserDocumentation)
+			{
+				string rootdir;
+				int indexOfBinInPath;
+				DetermineIndexOfBinInExecutablesPath(out rootdir, out indexOfBinInPath);
+				String basedir;
+				if (indexOfBinInPath >= 0)
+					basedir = rootdir.Substring(0, indexOfBinInPath);
+				else
+					basedir = rootdir;
+				Process.Start(Path.Combine(basedir, "doc", "UserDocumentation.pdf"));
+			}
+		}
+
+		private static void DetermineIndexOfBinInExecutablesPath(out string rootdir, out int indexOfBinInPath)
+		{
+			Uri uriBase = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+			rootdir = Path.GetDirectoryName(Uri.UnescapeDataString(uriBase.AbsolutePath));
+			indexOfBinInPath = rootdir.LastIndexOf("bin");
+		}
+
+		void PCPATRDoc_Click(object sender, EventArgs e)
+		{
+			ToolStripItem menuItem = (ToolStripItem)sender;
+			if (menuItem.Name == PCPATRReferenceManual)
+			{
+				string rootdir;
+				int indexOfBinInPath;
+				DetermineIndexOfBinInExecutablesPath(out rootdir, out indexOfBinInPath);
+				String basedir;
+				if (indexOfBinInPath >= 0)
+					basedir = rootdir.Substring(0, indexOfBinInPath);
+				else
+					basedir = rootdir;
+				Process.Start(Path.Combine(basedir, "doc", "pcpatr.html"));
+			}
+		}
+
+		void About_Click(object sender, EventArgs e)
+		{
+			ToolStripItem menuItem = (ToolStripItem)sender;
+			if (menuItem.Name == About)
+			{
+				var dialog = new AboutBox();
+				// for some reason the following is needed to keep the dialog within the form
+				Point pt = dialog.PointToClient(System.Windows.Forms.Cursor.Position);
+				dialog.Location = new Point(this.Location.X + 20, this.Location.Y + 20);
+				Console.WriteLine("dialog result=" + dialog.Location.X + "," + dialog.Location.Y);
+				dialog.Show();
+			}
+		}
+
+	}
+
+	public class SegmentToShow
+	{
+		public ISegment Segment { get; set; }
+		public String Baseline { get; set; }
+
+		public SegmentToShow(ISegment segment, string baseline)
+		{
+			Segment = segment;
+			Baseline = baseline;
+		}
+	}
+}
