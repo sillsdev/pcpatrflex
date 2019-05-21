6a3970779b16de8cdbb68b290f0510338f922417
andy black <blackhandrew@gmail.com>
2019-05-21 16:08:11 -0700

    Add Ana2XmlPrep

---
+++ Ana2XmlPrep/Ana2XmlPrep.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ana2XmlPrep
+{
+	class Ana2XmlPrep
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 4)
+			{
+				ShowUsage();
+			}
+			if (args[0] != "-i")
+			{
+				ShowUsage();
+			}
+			string InputFileName = args[1];
+			if (!File.Exists(InputFileName))
+			{
+				Console.Error.WriteLine("Input file '" + InputFileName + "' not found.");
+				Environment.Exit(1);
+			}
+			if (args[2] != "-o")
+			{
+				ShowUsage();
+			}
+			string OutputFileName = args[3];
+			string Contents = File.ReadAllText(InputFileName, Encoding.UTF8);
+			string Results = Contents.Replace("\r\n\r\n\\parse", "\r\n\\parse").Replace("\r\n\r\n\\failure", "\r\n\\failure");
+			File.WriteAllText(OutputFileName, Results, Encoding.UTF8);
+		}
+
+		private static void ShowUsage()
+		{
+			Console.Error.WriteLine("Usage: Ana2XmlPrep -i input_file -o output_file");
+			Environment.Exit(1);
+		}
+	}
+}
