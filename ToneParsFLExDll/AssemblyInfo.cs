d7581c3a66436eb16038c075f2fdc50280aa48ce
Andy Black <blackhandrew@gmail.com>
2024-06-26 08:47:05 -0700

    Version 0.6.1

--- ToneParsFLExDll/AssemblyInfo.cs
+++ ToneParsFLExDll/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SIL International")]
 [assembly: AssemblyProduct("Use TonePars with FLEx")]
-[assembly: AssemblyCopyright("Copyright © 2019-2023")]
+[assembly: AssemblyCopyright("Copyright © 2019-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
